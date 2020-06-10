0a8a10635321a4076970935ea68b2f6d788dc014
waelAhmedd <waelahmed2098@gmail.com>
2020-06-10 23:12:13 +0200

    add env-cmd

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 // \Users\WaelAhmed\mongoDp\bin\mongod.exe --dbpath=\Users\WaelAhmed\mongodbData
 //npm run dev
 const express =require('express')
-require('./db/mongoose')
+require('./db/mongose')
 
 
 
 const app = express()
 const port =3000
 
-app.use(express.json())
+//app.use(express.json())
 //app.use(userRouter)
 
 
 
-app.listen(port,()=>{
+/*app.listen(port,()=>{
     console.log('server'+port)
-})
+})*/
+console.log('hola')
